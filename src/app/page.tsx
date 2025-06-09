bccce1b5dc1823213ba1cc7714b5102176fa3fc6
Thiago-miotti <thiago.miotti10@gmail.com>
2025-06-09 10:00:42 -0300

    Added a build folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Tabs from "./components/Tabs";
@@ -42,7 +42,9 @@ export default function Home() {
   return (
     <div className="space-y-10 md:space-y-16">
       <Header />
-      <Tabs />
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Tabs />
+      </Suspense>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {extensions.map((extension) => (
           <Card
